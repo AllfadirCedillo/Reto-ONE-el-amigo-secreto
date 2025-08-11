5c6eb4606ab72795fea50b172b2a4068108d5eb7
Allfadir C <91344256+AllfadirCedillo@users.noreply.github.com>
2025-08-10 18:57:08 -0600

    Agrega funcionalidad de agregar y sortear amigos con JS

---
+++ README.md
@@ -0,0 +1,3 @@
+# Permitir ingresar nombres de sus amigos atraves de un campo de texto y un boton añadir
+# Realizar un sorteo entre los amigos ingresados para determinar quien es el amigo  secreto
+# Debe validar los datos de entrada para evitar los nombres de amigo vacio
---
+++ app.js
@@ -0,0 +1,21 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let inputAmigos = document.getElementById("amigo");
+let listaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+let arrayAmigos = [];
+
+//declaramos las funciones para agregar y sortear amigos
+function agregarAmigo() {
+	let nuevoAmigo = document.createElement("p");
+	nuevoAmigo.textContent = inputAmigos.value;
+	arrayAmigos.push(inputAmigos.value);
+	listaAmigos.appendChild(nuevoAmigo);
+	inputAmigos.value = "";
+}
+
+function sortearAmigo() {
+	// se genera un número aleatorio para elegir un amigo al azar por su indice
+	let numeroAleatorio = Math.floor(Math.random()*arrayAmigos.length);
+	resultado.textContent = arrayAmigos[numeroAleatorio];
+	console.log(numeroAleatorio);
+}
